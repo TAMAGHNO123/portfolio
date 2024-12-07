bda562e3c7099826dedd57aca1a7a34b60db717d
@tamaghno <tamaghnog@gmail.com>
2024-12-08 00:14:43 +0530

    Add clipboard functionality to About section for email copying

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Globe from 'react-globe.gl'
 import Button from '../components/Button'
 
 const About = () => {
+    const [hasCopied, setHasCopied] = useState(false);
     const handleCopy = () => {
         navigator.clipboard.writeText('tamaghnog@gmail.com');
+        setHasCopied(true);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
     }
     return (
         <section className='c-space my-20'>
@@ -74,7 +79,8 @@ bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
                             <div className='space-y-2'>
                                     <p className='grid-subtext text-center'>Contact Me</p>
                                     <div className='copy-container' onClick={handleCopy}>
-
+                                            <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt='copy' />
+                                            <p className='lg:text-2xl md:text-xl font-medium text-gray_gradient text-white '>tamaghnog@gmail.com</p>
                                     </div>
                             </div>
                     </div>
