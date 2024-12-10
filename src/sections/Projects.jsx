b98690d767f04e655a9cf57c3801a7b7c4a31c3e
@tamaghno <tamaghnog@gmail.com>
2024-12-10 11:27:47 +0530

    Add 3D visualization to Projects section with react-three/fiber integration

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react'
-import { myProjects } from '../constants/index.js'
+import React, { useState, Suspense } from 'react';
+import { myProjects } from '../constants/index.js';
+import { Canvas } from '@react-three/fiber';
+import { Center, OrbitControls } from '@react-three/drei';
+import CanvasLoader from '../components/CanvasLoader';
+import DemoComputer from '../components/DemoComputer.jsx';
 
 const projectCount = myProjects.length;
 
@@ -61,32 +65,38 @@ const Projects = () => {
           </div>
 
           <div className='flex justify-between items-center mt-7'>
-          <button className='arrow-btn' onClick={() => handleNavigation('previous')}>
-    <img 
-      src="/assets/right-arrow.png" 
-      alt="left arrow" 
-      className='w-4 h-4 rotate-180' // Added rotate-180
-    />
-  </button>
+            <button className='arrow-btn' onClick={() => handleNavigation('previous')}>
+              <img 
+                src="/assets/right-arrow.png" 
+                alt="left arrow" 
+                className='w-4 h-4 rotate-180' 
+              />
+            </button>
             <button className='arrow-btn' onClick={() => handleNavigation('next')}>
               <img src="/assets/right-arrow.png" alt="right arrow" className='w-4 h-4' />
             </button>
           </div>
         </div>
 
+        <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full'>
+          <Canvas>
+            <ambientLight intensity={Math.PI} />
+            <directionalLight position={[10, 10, 5]} intensity={1} />
+            <Center>
+              <Suspense fallback={<CanvasLoader />}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                      <DemoComputer texture={currentProject.texture}/>
+                </group>
+              </Suspense>
+            </Center>
 
 
 
 
 
-
-
-
-
-
-
-
-        
+            <OrbitControls maxPolarAngle={Math.PI/2} enableZoom={false}/>
+          </Canvas>
+        </div>
       </div>
     </section>
   );
